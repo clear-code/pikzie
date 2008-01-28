10fd42c01220c327567a328bf0831dd7d8976f57
Kouhei Sutou <kou@cozmixng.org>
2008-01-28 01:45:56 +0000

    * added upload_doc command.

--- setup.py
+++ setup.py
@@ -2,12 +2,40 @@
 
 import sys
 import os
+import subprocess
 
 from distutils.core import setup
+from distutils.cmd import Command
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "lib"))
 
 import pikzie
+
+sf_user = "ktou"
+sf_host = "%s@shell.sourceforge.net" % sf_user
+sf_htdocs = "/home/groups/p/pi/pikzie/htdocs"
+
+class upload_doc(Command):
+    description = "upload documentation"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        self._run("scp", "html/index.html",
+                  "%s:%s/index.html" % (sf_host, sf_htdocs))
+        self._run("rst2html", "README", "html/readme.html")
+        self._run("rst2html", "README.ja", "html/readme.html.ja")
+        self._run("scp", "html/readme.html", "html/readme.html.ja",
+                  "%s:%s/" % (sf_host, sf_htdocs))
+
+    def _run(self, *command):
+        if subprocess.call(command) != 0:
+            raise "failed to run: %s" % " ".join(command)
 
 setup(name="pikzie",
       version=pikzie.version,
@@ -21,4 +49,5 @@ setup(name="pikzie",
       classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Software Development :: Testing",
-        ])
+        ],
+      cmdclass={"upload_doc": upload_doc})
