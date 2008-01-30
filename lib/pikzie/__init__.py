359b049aff27e8c204f702be1dabbf776f79c1b3
Kouhei Sutou <kou@cozmixng.org>
2008-01-30 07:35:34 +0000

    * supported Python 2.3.

--- lib/pikzie/__init__.py
+++ lib/pikzie/__init__.py
@@ -1,4 +1,11 @@
 version = "0.2.0"
 
+if not hasattr(__builtins__, "sorted"):
+    def sorted(iterable):
+        list = iterable[:]
+        list.sort()
+        return list
+    __builtins__.sorted = sorted
+
 from pikzie.tester import Tester
 from pikzie.core import *
