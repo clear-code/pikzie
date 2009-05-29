c8fee5259c6322405b3dd96348386667cd376512
Kouhei Sutou <kou@cozmixng.org>
2009-05-29 04:18:15 +0000

    * add license block.

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+#
+# Copyright (C) 2009  Kouhei Sutou <kou@clear-code.com>
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 import os
@@ -238,7 +253,7 @@ setup(name=package_name,
       description=description,
       long_description=long_description,
       author="Kouhei Sutou",
-      author_email="kou@cozmixng.org",
+      author_email="kou@clear-code.com",
       url="http://pikzie.sourceforge.net/",
       download_url=download_url,
       license="LGPL",
