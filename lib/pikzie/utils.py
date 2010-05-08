4bfcc65a4d7b4fb62b26c7675ee294e7360f0281
Kouhei Sutou <kou@cozmixng.org>
2010-05-08 12:08:11 +0000

    * add utils: mkdir_p and rm_rf.

---
+++ lib/pikzie/utils.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2010  Kouhei Sutou <kou@clear-code.com>
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+__all__ = ["mkdir_p", "rm_rf"]
+
+def mkdir_p(path, *arguments, **parameters):
+    os.makedirs(path, *arguments, **parameters)
+
+def rm_rf(path):
+    if not os.path.exists(path):
+        return
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+    if os.path.isdir(path):
+        os.rmdir(path)
+    else:
+        os.remove(path)
---
+++ test/test_utils.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2010  Kouhei Sutou <kou@clear-code.com>
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import pikzie
+from pikzie.utils import *
+
+tmp_dir = os.path.join(os.path.dirname(__file__), "tmp")
+
+def setup():
+    rm_rf(tmp_dir)
+
+def teardown():
+    rm_rf(tmp_dir)
+
+def test_mkdir_p():
+    deep_dir = os.path.join(tmp_dir, "deep", "dir")
+    assert_not_exists(deep_dir)
+    mkdir_p(deep_dir)
+    assert_exists(deep_dir)
+
+def test_rm_f():
+    deep_dir = os.path.join(tmp_dir, "deep", "dir")
+    deep_file = os.path.join(deep_dir, "file")
+    mkdir_p(deep_dir)
+    assert_open_file(deep_file, "w")
+    assert_exists(tmp_dir)
+    rm_rf(tmp_dir)
+    assert_not_exists(tmp_dir)
