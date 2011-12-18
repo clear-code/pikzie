d8017aa7dfdd970b138ddd0e9220f29cf87c9af1
Kouhei Sutou <kou@clear-code.com>
2011-12-18 16:08:57 +0900

    1.0.0 -> 1.0.1.

--- lib/pikzie/__init__.py
+++ lib/pikzie/__init__.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-version = "1.0.0"
+version = "1.0.1"
 
 try:
     import __builtin__
