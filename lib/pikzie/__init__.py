f0db90e9b16c73fda667d3e12b8e54f5f845e3df
Kouhei Sutou <kou@clear-code.com>
2016-07-02 22:46:23 +0900

    Bump version

--- lib/pikzie/__init__.py
+++ lib/pikzie/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2009-2015  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2009-2016  Kouhei Sutou <kou@clear-code.com>
 #
 # This library is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-version = "1.0.2"
+version = "1.0.3"
 
 try:
     import __builtin__
