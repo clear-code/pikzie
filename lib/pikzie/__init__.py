fe593c7582584f567e5651482348d2195b77881e
Kouhei Sutou <kou@clear-code.com>
2016-07-29 09:25:34 +0900

    Bump version

--- lib/pikzie/__init__.py
+++ lib/pikzie/__init__.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-version = "1.0.3"
+version = "1.0.4"
 
 from pikzie.tester import Tester
 from pikzie.core import *
